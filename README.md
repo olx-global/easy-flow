6e9a68f589ce77edd2855c8106537965c8e6d73b
Fernando Otero (ZeoS) <fernando.otero@olx.com>
2018-07-19 14:44:03 -0300

    initial version

---
+++ README.md
@@ -0,0 +1,81 @@
+# Easy Flow 
+
+`easy_branch`: A simple tool to create a Jira issue, a branch with the issue id and summary.
+
+### Prerequisites
+
+jq
+curl
+Jira key (https://id.atlassian.com/manage/api-tokens)
+
+## easy_branch
+
+
+### Getting Started
+
+Export these variables with their proper values:
+
+```
+export JIRA_KEY=[key]
+export JIRA_USER=[user]
+export JIRA_SERVER=[org].atlassian.net
+export JIRA_IN_PROGRESS_ID=get it from jira, default value if absent: 111
+```
+
+You might want to symlink the script:
+```
+ln -s ~/easy-flow/easy-flow.sh easy-flow
+```
+
+Source it:
+```
+source easy-flow 
+```
+
+use it:
+```
+easy_branch this is a test summary
+```
+
+and add to your `.bashrc` or `.zshrc` and maybe alias for short:
+
+```
+export JIRA_KEY=...
+export JIRA_USER=[user]
+source easy-flow
+alias eb=easy_branch
+```
+
+Most of the times you'll want to have the squad by default (and maybe a label?)
+```
+alias eb="easy_branch -l some_label"
+```
+
+
+### Parameters:
+```
+-t [Bug|Task(Default)|Story]
+-d Description (optional)
+-nb | --nobranch Does not create a branch
+-l [label] (can be used mutiple times: -l label1 -l label_2 ...)
+* summary
+```
+If you want to create a bug you can do (:
+```
+eb -t Bug this is a summary
+```
+or if you want descriptions:
+
+```
+eb -t Bug -d "this is the description" this is the summary
+```
+
+note that descriptions have quotes
+
+## Jira Transition
+Move tickets to "In Progress" :
+```
+to_in_progress [TICKET-ID]
+```
+
+
---
+++ easy-flow.sh
@@ -0,0 +1,121 @@
+######################################################
+# to_in_progress transitions a ticket to "IN PROGRESS"
+# $1 : ticket id
+#####################################################
+function to_in_progress {
+     TICKET=$1
+     ##in our case 111 is the id of "in progress"
+     [ -z "$JIRA_IN_PROGRESS_ID" ] && export JIRA_IN_PROGRESS_ID=111
+     CURLDATA='{"transition":{"id":'"$JIRA_IN_PROGRESS_ID"'}}'
+     RESPONSE=`curl --request POST \
+	  --user "$JIRA_USER":"$JIRA_KEY"\
+	  --header 'Accept: application/json' \
+	  --header 'Content-Type: application/json' \
+	  --data "$CURLDATA"\
+	  --url 'https://'"$JIRA_SERVER"'/rest/api/2/issue/'"$TICKET"'/transitions'`
+}
+
+ #######################################################
+# easy_branch creates a ticket and a branch (by default) #  
+ #######################################################
+function easy_branch {
+
+	if [ -z "$JIRA_USER" ]; then
+		echo "ERROR: Please set JIRA_USER env variable with your username for jira (usually an email)"
+		return
+	fi
+	if [ -z "$JIRA_KEY" ]; then
+		echo "ERROR: Please set JIRA_KEY env variable. You can get the key here: https://id.atlassian.com/manage/api-tokens" 
+		return
+	fi
+	if [ -z "$JIRA_SERVER" ]; then
+		echo "ERROR: Please set JIRA_SERVER env variable. Usually org.atlassian.com" 
+		return
+	fi
+	POSITIONAL=()
+	TYPE="Task"
+	SUMMARY=""
+	DESCRIPTION=""
+	LABELS=()
+	TEAMS=()
+	SQUAD=()
+	DO_BRANCH=true
+
+	shift $((OPTIND-1))
+	while [[ $# -gt 0 ]]
+	do
+	key="$1"
+
+	case $key in
+	    -t|--type)
+	    TYPE="$2"
+	    shift # pass argument
+	    shift
+	    ;;
+	    -d|--description)
+	    DESCRIPTION="$2"
+	    shift 
+	    ;;
+	    -l|--label)
+	    LABELS+=("$2")
+	    shift 
+	    shift
+	    ;;
+	    -nb|--nobranch)
+	    DO_BRANCH=false
+	    shift 
+	    ;;
+	    *)    # unknown option
+	    if [ -z "$SUMMARY" ]; then
+	      SUMMARY=("$1") # save it in an array for later
+	    else
+	      SUMMARY=("$SUMMARY $1")
+	    fi
+	    shift 
+	    ;;
+	esac
+	done
+	if [ -z "$LABELS" ]; then
+	    LABELS_JSON="[]"
+	else
+	    LABELS_JSON=$(printf '%s\n' "${LABELS[@]}" | jq -R . | jq -sc .)
+	fi
+	set -- "${POSITIONAL[@]}" # restore positional parameters
+	CURLDATA='{
+		    "fields": {
+		       "project":
+		       {
+			  "key": "PAN"
+		       },
+		       "summary": "'$SUMMARY'",
+		       "description": "'$DESCRIPTION'",
+		       "issuetype": {
+			  "name": "'$TYPE'"
+		       },
+		       "labels" : '$LABELS_JSON'
+		   }
+		}
+	    '
+	#echo $CURLDATA
+	RESPONSE=`curl --request POST \
+	  --user "$JIRA_USER":"$JIRA_KEY"\
+	  --header 'Accept: application/json' \
+	  --header 'Content-Type: application/json' \
+	  --data "$CURLDATA"\
+	  --url 'https://'"$JIRA_SERVER"'/rest/api/2/issue/'`
+	ISSUE_KEY=$(echo "$RESPONSE" | jq -cr .key)
+	if [[ "$ISSUE_KEY" == "null" ]]; then
+		echo "ERROR: $RESPONSE"
+	        return	
+	fi
+	if [ "$DO_BRANCH" = true ]; then
+		SUMMARY_BRANCH=${SUMMARY// /_}
+		BRANCH="$ISSUE_KEY""_""$SUMMARY_BRANCH"
+		git branch $BRANCH
+		git checkout $BRANCH
+		to_in_progress $ISSUE_KEY
+	else
+	        echo "CREATED TICKET: $ISSUE_KEY"
+	fi
+}
+
